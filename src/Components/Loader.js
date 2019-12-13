8d0163703547aea40e43d1d5936cf62d640995f6
Park Jongho <kim010c@gmail.com>
2019-12-13 16:17:19 +0900

    #7-5 toggleLike on Post Component

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -15,11 +15,16 @@ const Animation = keyframes`
 `;
 
 const Loader = styled.div`
-  animation: ${Animation} 1s linear infinite;
+  /* animation: ${Animation} 1s linear infinite; */
+  min-height: 70vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0.3;
 `;
 
 export default () => (
   <Loader>
-    <Logo size={36} />
+    <Logo size={50} />
   </Loader>
 );
